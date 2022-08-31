16e7ef776b347ec25b949a20c6058c89d3b3485f
Huzaifa109837 <72577696+Huzaifa109837@users.noreply.github.com>
2022-09-01 01:32:42 +0600

    week11-funa

---
+++ week11/main.js
@@ -0,0 +1,14 @@
+var p = document.getElementsByClassName('preloader-box');
+p[0].style.animation = 'rotate 1.5s ease-in-out infinite';
+p[1].style.animation = 'rotate 1.5s cubic-bezier(0.23, 0.03, 0.77, 1) infinite';
+p[2].style.animation = ' rotate 1.5s linear infinite';
+
+
+const loading = document.querySelector('.preloader');
+const content = document.querySelector('#content');
+content.style.display = 'none';
+setTimeout(() => {
+  content.style.display = 'block';
+    loading.style.display = 'none';
+    }, 2000)
+    
